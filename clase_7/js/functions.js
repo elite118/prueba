3838bec6154e2c533b136b611902e9ced0e99a87
elite118 <fseo118@gmail.com>
2023-04-24 19:54:30 -0500

    first commit

---
+++ clase_7/js/functions.js
@@ -0,0 +1,16 @@
+function fechaActual(){
+    document.getElementById('table').style.display="block";
+    //Select div
+    
+    const div = document.querySelector('.fecha');
+
+    // Apply style to div
+    div.setAttribute('style', 'display: block');
+
+    document.querySelector('div').setAttribute('style', 'display: block');
+}
+
+function agregarHTML(){
+    document.getElementById('mi_contenedor').innerHTML = ("<p>esto es un parrafo<p/>");
+
+}
